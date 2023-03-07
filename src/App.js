12a02baff176b92122b13b0b21dd972ddf0f5a3f
Mohit <laharimohit87@gmail.com>
2023-03-07 13:20:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import ForeCast from './Components/ForeCast';
+import Inputs from './Components/Inputs';
+import TemperatureAndDetails from './Components/TemperatureAndDetails';
+import TimeAndLocation from './Components/TimeAndLocation';
+// import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './Components/TopButtons';
+import getFormattedWeatherData from './Services/WeatherService';
+// import getWeatherData from './Services/WeatherService';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+function App() {
+
+const [query, setQuery]= useState({q:'indore'})
+const [units, setUnits]= useState('metric')
+const [weather,setWeather]= useState(null)
+
+
+useEffect(()=>{
+  const fetchWeather = async ()=>{
+const message = query.q ? query.q : 'current location.'
+
+toast.info('Fetching weather for ' + message)
+
+    await getFormattedWeatherData({...query, units})
+    .then((data)=>{
+      toast.success(`Successfully fetched weather  for ${data.name},${data.country}.`)
+      setWeather(data)
+    })
+    // console.log(data)
+   }
+   
+   fetchWeather()
+   
+},[query,units])
+
+const formatBackground=()=>{
+  if(!weather) return 'from-cyan-700 to-blue-700'
+  const thresold = units === 'metric' ? 20 : 60
+
+  if(weather.temp <= thresold) return 'from-cyan-700 to-blue-700'
+
+  return 'from-yellow-700 to-orange-700'
+}
+
+
+
+
+  return (
+    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fits shadow-xl shadow-gray-400 ${formatBackground()}`}>
+     <TopButtons setQuery={setQuery} />
+     <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
+{
+ weather && (
+<div>
+  <TimeAndLocation weather={weather}/>
+  <TemperatureAndDetails weather={weather} />
+  <ForeCast title="hourly forecast" items={weather.hourly} />
+  <ForeCast title="daily forecast" items={weather.daily} />
+  </div>
+ )
+}
+<ToastContainer autoClose={2000} theme="colored" newestOnTop={true} />
+     </div>
+
+  );
+}
+
+export default App;
